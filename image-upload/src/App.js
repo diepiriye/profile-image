46e4c25a572c92ff78dc10655450b3029718cc76
didibaebi <diepiriyeariel@gmail.com>
2023-11-13 15:53:35 +0100

    --

--- image-upload/src/App.js
+++ image-upload/src/App.js
@@ -1,44 +1,60 @@
-import logo from './logo.svg'
+
 import { useState } from 'react'
 
+
 function App() {
-  const [photo, setPhoto] = useState();
+  const [photo, setPhoto] = useState([]);
   function changeMe(e){
-    setPhoto(URL.createObjectURL(e.target.files[0]))
-    
+    const pictures = e.target.files;
+    console.log(pictures)
+    const pictureArray = Array.from(pictures)
+    console.log(pictureArray)
+    const pictureMap = pictureArray.map((file)=>{
+      return URL.createObjectURL(file)
+    })
+    console.log(pictureMap)
+    setPhoto(pictureMap)
   }
-  function drophere(e){
-    setPhoto(URL.createObjectURL(e.target.files[0]))
+  
+  function removeMe(index){
+    const own = photo.filter((value,index,arr)=> arr.indexOf(value) !== index )
+    setPhoto(own);
   }
-
-
-
-
+  
   return (
     <div className="App">
       <div className="container">
         <div className="clean-mock">
-        <i class="fa-solid fa-camera-retro"></i>
+        <i className="fa-solid fa-camera-retro"></i>
           <p>Clean mock</p>
         </div>
         <div className="main-container">
           <div className="form-container">
             <div className="card">
               <div className="card-image">
-                <img src={photo} alt="" className="card-pic"/>
+                <img src="./images/drop.png" alt="" className="card-pic"/>
               </div>
               <h4>Drag & Drop</h4>
               <span>Upload only png, jpg, or jpeg</span>
-              <label for="upload-file">Upload Image</label>
+              <label htmlFor="upload-file">Upload Image</label>
               <input type="file"
-               accept="image/png, image/jpg, image/jpeg" 
+               multiple accept="image/png, image/jpg, image/jpeg" 
                id="upload-file" 
                onChange={changeMe}
-               onDrop={drophere}
                />
             </div>
           </div>
         </div>   
+      </div>
+      <div className="picture-space">
+        {photo && photo.map((pic,index,arr)=>{
+          return(
+          <div className="picture-image-container" key={pic.toString()}>
+            <img src={pic} alt="" className="picture-image"/>
+            <button className="delete" onClick={()=>removeMe(arr.indexOf(pic))}>Delete</button>
+          </div>
+          );
+        })}  
       </div>
     </div>
   );
